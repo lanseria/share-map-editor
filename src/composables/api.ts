92b7b4177b8a8e37fcbd21a1bc3b223d789c1366
lanseria <zhangchao564265135@hotmail.com>
2022-12-18 21:39:22 +0800

    fix api url

--- src/composables/api.ts
+++ src/composables/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 // https://8000.vip.cpolar.top/api/
 // http://127.0.0.1:8080/
-// const prefix = 'https://8000.vip.cpolar.top/api'
-const prefix = 'http://127.0.0.1:8080'
+const prefix = 'https://8000.vip.cpolar.top/api'
+// const prefix = 'http://127.0.0.1:8080'
 export const queryMapFeatures = (data: any) => {
   return axios.request({
     url: `${prefix}/map/featuresbounds`,
